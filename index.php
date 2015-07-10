90e03bbb4c5254cf1d6b1e0fe7421bab4e182ddf
1112360-Truong Dep Trai <1112360@student.hcmus.edu.vn>
2015-07-10 10:40:31 +0000

    Make yii skeletonized and redirect index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+header('HTTP/1.1 307 Temporary Redirect');
+header('Location: projectDelta/');
+?>
+<html>
+  <head>
+    <title>Moved</title>
+  </head>
+  <body>
+    <h1>Moved</h1>
+    <p>This page has moved to <a href="projectDelta/">projectDelta</a>.</p>
+  </body>
+</html>
